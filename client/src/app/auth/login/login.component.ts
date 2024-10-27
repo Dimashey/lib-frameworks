b8ff6c9811dfbbb88eb19a2eb93e4e40e7032093
DmytroSheiko <dmytro.sheiko@sigma.software>
2024-10-27 21:11:18 +0200

    feat: create login form

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
+import { MatCard, MatCardContent } from '@angular/material/card';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatSlideToggleModule],
+  imports: [MatCard, MatCardContent, MatFormField, MatLabel, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  user: string = '';
+  password: string = '';
+
+  constructor() {}
+
+  login(): void {
+    console.log(this.user, this.password)
+  }
+}
